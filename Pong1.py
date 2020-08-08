93a4d43242bcaf581cb93a33c0ecf6e9aef08541
Kevin McCarthy <km.mccarthy11@gmail.com>
2020-08-08 13:10:05 -0400

    added ball movements and x y border checks

--- Pong1.py
+++ Pong1.py
@@ -34,6 +34,8 @@ ball.shape("square")
 ball.color("white")
 ball.penup()
 ball.goto(0, 0)
+ball.dx = 2 #d as in delta, 2px
+ball.dy = -2
 
 #Functions
 def paddle_a_up():
@@ -71,3 +73,20 @@ wn.onkey(paddle_b_down, "Down")
 
 while True:
     wn.update()
+
+    #move the ball
+    ball.setx(ball.xcor() + ball.dx)
+    ball.sety(ball.ycor() + ball.dy)
+
+    #border checking
+    if ball.ycor() > 290:
+        ball.sety(290)
+        ball.dy *= -1 #reverses direction of ball
+
+    if ball.ycor() < -290:
+        ball.sety(-290)
+        ball.dy *= -1 
+
+    if ball.xcor() > 390:
+        ball.goto(0, 0)
+        ball.dx *= -1
