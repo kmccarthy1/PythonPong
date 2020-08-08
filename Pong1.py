32081f0f8e91513a459bcb473df7600f4b4c34da
Kevin McCarthy <km.mccarthy11@gmail.com>
2020-08-08 14:43:25 -0400

    added sounds

--- Pong1.py
+++ Pong1.py
@@ -1,4 +1,5 @@
 import turtle
+import os
 #turtle for graphics, open windows, etc
 
 #wn=window
@@ -7,6 +8,12 @@ wn.title ("Pong by @KMcCarthy")
 wn.bgcolor("black")
 wn.setup(width=800, height=600)
 wn.tracer(0)
+
+
+#Score
+score_a = 0
+score_b = 0
+
 
 #Paddle A 
 paddle_a = turtle.Turtle()
@@ -36,6 +43,16 @@ ball.penup()
 ball.goto(0, 0)
 ball.dx = 2 #d as in delta, 2px
 ball.dy = -2
+
+#Pen
+pen = turtle.Turtle()
+pen.speed(0)
+pen.color("white")
+pen.penup()
+pen.hideturtle()
+pen.goto(0, 260)
+pen.write("Player A: 0   Player B: 0", align="center", font=("Courier", 24, "normal"))
+
 
 #Functions
 def paddle_a_up():
@@ -82,11 +99,39 @@ while True:
     if ball.ycor() > 290:
         ball.sety(290)
         ball.dy *= -1 #reverses direction of ball
+        os.system("afplay cartoon-jump.wav&")
 
     if ball.ycor() < -290:
         ball.sety(-290)
         ball.dy *= -1 
+        os.system("afplay cartoon-jump.wav&")
+
 
     if ball.xcor() > 390:
         ball.goto(0, 0)
         ball.dx *= -1
+        score_a += 1
+        pen.clear()
+        pen.write("Player A: {}   Player B: {}".format(score_a, score_b), align="center", font=("Courier", 24, "normal"))
+        os.system("afplay clank-cartoon.wav&")
+
+    if ball.xcor() < -390:
+        ball.goto(0, 0)
+        ball.dx *= -1
+        score_b += 1
+        pen.clear()
+        pen.write("Player A: {}   Player B: {}".format(score_a, score_b), align="center", font=("Courier", 24, "normal"))
+        os.system("afplay clank-cartoon.wav&")
+
+
+    #paddle ball collision
+    if (ball.xcor() > 340 and ball.xcor() < 350) and (ball.ycor() < paddle_b.ycor() + 40 and ball.ycor() > paddle_b.ycor() -40):
+        ball.setx(340)
+        ball.dx *= -1
+        os.system("afplay cartoon-jump.wav&")
+
+
+    if (ball.xcor() < -340 and ball.xcor() > -350) and (ball.ycor() < paddle_a.ycor() + 40 and ball.ycor() > paddle_a.ycor() -40):
+        ball.setx(-340)
+        ball.dx *= -1
+        os.system("afplay cartoon-jump.wav&")
